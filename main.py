1aae9d25ea427844fc510ad2bed84931f1994c13
ikanberjalandidarat <46159013+ikanberjalandidarat@users.noreply.github.com>
2021-01-07 11:02:08 +0700

    Added files

---
+++ main.py
@@ -0,0 +1,24 @@
+# Zhafira Elham 1/21
+
+import os
+# import config
+
+
+def get_words(file_path):
+    with open(file_path, 'r') as f:
+        # print(f.readlines())
+        text = f.readlines()[0]
+        #print(text.split())
+        words = text.split()
+    return words
+
+def send_message(direct, message):
+    os.system('osascript send.scpt {} "{}"'.format(direct, message))
+    # testing line = os.system('osascript send.scpt 087883074199 "Move to main.py rather than terminal test1"')
+
+
+if __name__ == '__main__':
+    words = get_words('test.txt')
+    receipent = 'gerardien.elham@gmail.com'
+    for word in words:
+        send_message(receipent, word)
